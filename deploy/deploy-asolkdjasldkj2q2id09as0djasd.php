600314a79e34c57c71a252c95e98f8f8d1da9d65
Alex <alex@rgclan.tk>
2014-09-22 02:03:39 -0500

    Updated deployer to pull proper repo + test line

--- deploy/deploy-asolkdjasldkj2q2id09as0djasd.php
+++ deploy/deploy-asolkdjasldkj2q2id09as0djasd.php
@@ -1,4 +1,5 @@
-<?php              
+<?php            
+// DEP TEST  
 /** 
  * Git Deployment Script for BitBucket
  *
@@ -22,13 +23,13 @@
 include('class.bitbucket-cron-deploy.php');
 
 // Where you have put your DETACHED HEAD repository
-$repo_path = '/home/alex/mobilePOS/mobilepos.git';
+$repo_path = '/home/alex/mobilePOS_dev/mobilepos.git';
 
 // Where you want to deploy the files to
-$root_path = '/home/alex/public_html/www.mobilepos.tk';
+$root_path = '/home/alex/public_html/dev.it.www.vm';
 
 // The name of the branch to deploy.
-$git_branch = 'master';
+$git_branch = 'develop';
 
 // If you need to specify the full path to the git binary (if it's not in your PATH)
 $git_path = '/usr/bin/git';
