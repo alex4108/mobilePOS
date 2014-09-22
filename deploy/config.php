69f9189f4d7166b377c339a534bc9c000b46ad18
Alex <alex@rgclan.tk>
2014-09-22 02:40:37 -0500

    Messed up configuration in master instance... fixing.

--- deploy/config.php
+++ deploy/config.php
@@ -4,13 +4,13 @@
 // If something needs to be changed, please modify config.php directly on the instance
 
 // Where you have put your DETACHED HEAD repository
-$repo_path = '/home/alex/mobilePOS_dev/mobilepos.git';
+$repo_path = '/home/alex/mobilePOS/mobilepos.git';
 
 // Where you want to deploy the files to
-$root_path = '/home/alex/public_html/dev.it.www.vm';
+$root_path = '/home/alex/public_html/www.mobilepos.tk';
 
 // The name of the branch to deploy.
-$git_branch = 'develop';
+$git_branch = 'master';
 
 // If you need to specify the full path to the git binary (if it's not in your PATH)
 $git_path = '/usr/bin/git';
